669cd630b252c1c7d07afb04f3429d72d76a5eef
JaimePrada11 <jandrespradato.11@gmail.com>
2025-02-22 23:00:15 -0500

    feat: :sparkles: Creacion de las clases principales

---
+++ Backend/src/domain/modelos/Categoria.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../../../config/database');
+
+
+class Categoria {
+    constructor(nombre) {
+        this._nombre = nombre;
+    }
+
+    get nombre() {
+        return this._nombre;
+    }
+
+    set nombre(newNombre) {
+        this._nombre = newNombre;
+    }
+
+
+    static async createFromData(categoryData) {
+        const category = new Categoria(categoryData.nombre);
+        return await CategoriaModel.create({
+            nombre: category.nombre
+        });
+    }
+}
+
+const CategoriaModel = sequelize.define('Categoria', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = { Categoria, CategoriaModel };
