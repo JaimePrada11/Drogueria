aa2791f53e1b4ffbe5a9f6ca08a96302edb8b36f
JaimePrada11 <jandrespradato.11@gmail.com>
2025-02-26 23:29:31 -0500

    feat: :sparkles: configuración database

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,32 +1,32 @@
 const { Sequelize } = require('sequelize');
-const config = require('./config');
 
-const sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, {
-    host: config.db.host,
-    port: config.db.port,
-    dialect: config.db.dialect,
-    logging: config.db.logging,
+const sequelize = new Sequelize('railway', 'root', 'AGXjoTeUHFtuoWmpNgaohouKgmjZOCgg', {
+    host: process.env.DB_HOST || 'yamabiko.proxy.rlwy.net',
+    port: process.env.DB_PORT || 15839,
+    dialect: 'mysql',
+    logging: false,
     dialectOptions: {
-        multipleStatements: true,
         ssl: {
             require: true,
             rejectUnauthorized: false
         }
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     }
 });
-
 
 async function connectDB() {
     try {
         await sequelize.authenticate();
-        console.log('✅ Conexión a la base de datos exitosa.');
+        console.log('✅ Conexión exitosa.');
     } catch (error) {
-        console.error('❌ Error al conectar con la base de datos:', error);
+        console.error('❌ Error de conexión:', error);
         process.exit(1);
     }
 }
 
-module.exports = {
-    sequelize,
-    connectDB
-};
+module.exports = { sequelize, connectDB };
