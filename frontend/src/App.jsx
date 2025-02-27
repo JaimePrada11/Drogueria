58e03ef4528606724fc2243bc5bd9bf7bbed4271
JaimePrada11 <jandrespradato.11@gmail.com>
2025-02-27 00:22:09 -0500

    fix: :bug: BrowserRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,24 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Inventario from "./pages/Inventario";
-import Catalogo from "./pages/Catalogo";
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './pages/home';
+import Inventario from './pages/Inventario';
+import Catalogo from './pages/Catalogo';
 
 function App() {
+
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/productos" element={<Catalogo />} />
-        <Route path="/inventario" element={<Inventario />} />
-      </Routes>
+      <Router>
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/productos' element={<Catalogo />} />
+          <Route path='/inventario' element={<Inventario />} />
+
+        </Routes>
+      </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
