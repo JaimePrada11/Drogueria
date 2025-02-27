5f8e7cc4f66f7ad0c6dd1845a9d3a30785cb979f
JaimePrada11 <jandrespradato.11@gmail.com>
2025-02-26 23:11:01 -0500

    feat: :sparkles: Despliegue Base de Datos

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -1,15 +1,16 @@
-require('dotenv').config(); 
+require('dotenv').config();
 
 const config = {
     app: {
         port: process.env.PORT || 3000,
     },
     db: {
-        host: process.env.DB_HOST || 'localhost',
-        username: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || '',
-        database: process.env.DB_NAME || 'drogueria',
-        dialect: process.env.DB_DIALECT || 'mysql', 
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,  
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        dialect: process.env.DB_DIALECT,
         logging: process.env.DB_LOGGING === 'true',
     }
 };
--- Backend/config/database.js
+++ Backend/config/database.js
@@ -3,20 +3,26 @@ const config = require('./config');
 
 const sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, {
     host: config.db.host,
+    port: config.db.port,
     dialect: config.db.dialect,
     logging: config.db.logging,
     dialectOptions: {
-        multipleStatements: true 
+        multipleStatements: true,
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
 });
+
 
 async function connectDB() {
     try {
         await sequelize.authenticate();
-        console.log('Conexión a la base de datos exitosa.');
+        console.log('✅ Conexión a la base de datos exitosa.');
     } catch (error) {
-        console.error('Error al conectar con la base de datos:', error);
-        process.exit(1);  
+        console.error('❌ Error al conectar con la base de datos:', error);
+        process.exit(1);
     }
 }
 
